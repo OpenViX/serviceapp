db31b0a6eefd2753be16c194b666789fdc9c1688
Huevos <rowland@moralet.com>
2025-04-13 05:19:57 +0200

    [Vu passthrough fix] don't require passthrough to be enabled because that is in another unrelated screen
    Obviously a passthrough option will only affect passthrough

--- src/plugin/plugin.py
+++ src/plugin/plugin.py
@@ -235,7 +235,7 @@ class ServiceAppSettings(ConfigListScreen, Screen):
         return config_list
     
     def serviceapp_passthrough_options(self, config_list):
-        if SystemInfo["Vu_EAC3_fix"] and config.av.downmix_ac3.value == "passthrough":
+        if SystemInfo["Vu_EAC3_fix"]:
             config_list.append(getConfigListEntry(_("Enable AC3+ passthrough fix"), config_serviceapp.passthrough_fix_enable, _("Enables AC3+ passthrough fix for Vu+ Ultimo4K / Duo4KSE.")))
             if config_serviceapp.passthrough_fix_enable.value:
                 config_list.append(getConfigListEntry(_("AC3+ Passthrough fix delay"), config_serviceapp.passthrough_fix_delay, _("Select the delay that will be used for AC3+ Passthrough fix.")))
